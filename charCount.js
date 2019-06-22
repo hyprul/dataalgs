b4378ce49800ccccb2d8a942bd11e83a12d48935
Sean Huang <hyprul@ucla.edu>
2019-06-22 16:27:28 -0400

    charcount different impl

--- charCount.js
+++ charCount.js
@@ -1,14 +1,24 @@
 function charCount(str) {
     var obj = {};
 
-    for (let i = 0; i < str.length; i++) {
-        let char = str[i].toLowerCase();
-        if (/[a-z0-9]/.test(char)) {
+    for (let char of str) {
+        if (isAlphaNumeric(char)) {
+            char = char.toLowerCase();
             obj[char] = ++obj[char] || 1;
         }
     }
 
     return obj;
+}
+
+function isAlphaNumeric(char) {
+    let code = char.charCodeAt(0);
+    if (!(code > 47 && code < 58) &&
+        !(code > 64 && code < 91) &&
+        !(code > 96 && code < 123)) {
+        return false;
+    }
+    return true;
 }
 
 console.log(charCount("Hello, hi, blah hello"))
