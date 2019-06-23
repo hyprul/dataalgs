9eb49c4880705a115c5c94c27a03a0e7095ed417
Sean Huang <hyprul@ucla.edu>
2019-06-22 21:54:12 -0400

    fixed comma'

--- slidingwindow.js
+++ slidingwindow.js
@@ -5,9 +5,10 @@ function maxSum(arr, n) {
     let maxSum = 0;
     let tempSum = 0;
 
-    for (let i = 0; i < n, i++) {
+    for (let i = 0; i < n; i++) {
         maxSum += arr[i];
     }
+
     tempSum = maxSum;
     for (let i = n; i < arr.length; i++) {
         tempSum = tempSum - arr[i - n] + arr[i];
